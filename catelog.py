a38fb2c9d9487225e1f89042a8a9860dca2e3490
tomas-20 <tacuna20@stuy.edu>
2022-04-04 08:19:58 -0400

    made it so that catelog.py renders the template with a category based on a querry string

--- catelog.py
+++ catelog.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from get_template import get_template
+from cgi import FieldStorage
+
+fields = FieldStorage()
 
 print("Content-type: text/html\n")
 
-print(get_template("temp.html").render())
+print(get_template("temp.html").render(category = fields["category"]))
