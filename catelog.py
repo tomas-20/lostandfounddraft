72f1ff7a71f4e85b63ba17ad00292414bce52703
tomas-20 <tacuna20@stuy.edu>
2022-04-11 10:52:13 -0400

    made it so that the catelog page gets items from the database

--- catelog.py
+++ catelog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from get_template import get_template
+from data_functions import get_items
 from cgi import FieldStorage
 
 category_names = {"outerwear": "Outerwear", "gloveshatsscarves": "Gloves, Hats, and Scarves", "personalschoolsupplies": "Personal School Supplies", "bookspapers": "Books, Textbooks, and Papers", "smallitems": "Small Items"}
@@ -10,4 +11,4 @@ print("Content-type: text/html\n")
 
 category = fields.getvalue("category")
 
-print(get_template("temp.html").render(category = category, category_name = category_names[category]))
+print(get_template("temp.html").render(category = category, category_name = category_names[category], items = get_items(category)))
--- data_test.py
+++ data_test.py
@@ -2,9 +2,9 @@ from data_functions import *
 
 if __name__ == "__main__":
     reset_data()
-    add_item("outwear", "1/15/2020", "a nice blue jacket", "www.jackets.com/jackets?color=blue", b"azuuuuullll")
-    add_item("outwear", "1/16/2020", "a nicer green jacket", "www.jackets.com/jackets?color=green", b"verrrrdeeeee")
-    add_item("outwear", "1/16/2020", "a mediocre orange jacket", "www.jackets.com/jackets?color=orange", b"narannnjaaaa")
+    add_item("outerwear", "1/15/2020", "a nice blue jacket", "https://www.jackets.com/jackets?color=blue", b"azuuuuullll")
+    add_item("outerwear", "1/16/2020", "a nicer green jacket", "https://www.jackets.com/jackets?color=green", b"verrrrdeeeee")
+    add_item("outerwear", "1/16/2020", "a mediocre orange jacket", "https://www.jackets.com/jackets?color=orange", b"narannnjaaaa")
     print(get_next_id())
     update_found(1, True)
-    print(get_items("outwear"))
+    print(get_items("outerwear"))
