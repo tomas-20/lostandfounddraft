c2803fe55213c887ef4a577d592e46d684c7993d
tomas-20 <tacuna20@stuy.edu>
2022-04-11 10:22:24 -0400

    made it so that the header on a catelog page varies off of the category

--- catelog.py
+++ catelog.py
@@ -2,8 +2,12 @@
 from get_template import get_template
 from cgi import FieldStorage
 
+category_names = {"outerwear": "Outerwear", "gloveshatsscarves": "Gloves, Hats, and Scarves", "personalschoolsupplies": "Personal School Supplies", "bookspapers": "Books, Textbooks, and Papers", "smallitems": "Small Items"}
+
 fields = FieldStorage()
 
 print("Content-type: text/html\n")
 
-print(get_template("temp.html").render(category = fields.getvalue("category")))
+category = fields.getvalue("category")
+
+print(get_template("temp.html").render(category = category, category_name = category_names[category]))
